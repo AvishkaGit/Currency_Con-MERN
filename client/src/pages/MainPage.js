28b3bd82ad101bdffedefed37b8ae881e6383cbc
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2024-03-28 19:57:11 +0530

    up date
    30%

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -3,8 +3,20 @@ import React from 'react'
 export default function MainPage() {
   return (
     <div>
-        <h1>Convert Your Currencies GUYs</h1>
-        <p>"Convert Your Currencies GUYs" suggests that you should exchange or change your money from one currency to another today. This could mean converting dollars to euros, yen to dollars, or any other currency pair. Doing so might help you take advantage of favorable exchange rates or meet specific financial needs.</p>
+        <h1 className=' lg:mx-32 text-5xl font-bold text-green-500'>Convert Your Currencies GUYs</h1>
+
+        <p className=' lg:mx-32 opacity-40 py-6'>"Convert Your Currencies GUYs" suggests that you should exchange or change your money from one currency to another today. This could mean converting dollars to euros, yen to dollars, or any other currency pair. Doing so might help you take advantage of favorable exchange rates or meet specific financial needs.</p>
+
+        <div>
+            <section>
+                <form>
+                    <div>
+                        
+                    </div>
+                </form>
+            </section>
+        </div>
+
     </div>
   )
 }

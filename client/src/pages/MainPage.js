867258e5ef1c2b64c0c560944ef07b160053c730
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2024-03-28 19:24:29 +0530

    update
    install backend pakeg and client side install tailwil css

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MainPage() {
+  return (
+    <div>
+        <h1>Convert Your Currencies GUYs</h1>
+        <p>"Convert Your Currencies GUYs" suggests that you should exchange or change your money from one currency to another today. This could mean converting dollars to euros, yen to dollars, or any other currency pair. Doing so might help you take advantage of favorable exchange rates or meet specific financial needs.</p>
+    </div>
+  )
+}
